5ff7e624828086e6327be395d762ea272724056b
joostlek <joostlek@outlook.com>
2022-05-26 18:59:29 +0200

    Fix importing

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 const requireAll: any = (r: any) => r.keys().forEach(r)
 
-requireAll(require.context("./components", true))
-requireAll(require.context("./utils", true))
+requireAll(require.context("./components", true, /\.vue|.ts$/))
+requireAll(require.context("./utils", true, /\.vue|.ts$/))
